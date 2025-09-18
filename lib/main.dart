b14a63e1db61dc4b43ad847ef4b69240216631c1
manishshrestha01 <shresthamanish8080@gmail.com>
2025-09-19 02:49:54 +0545

    Counter App Finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomeScreen()));
+  runApp(MaterialApp(home: CounterScreen()));
 }
 
 class HomeScreen extends StatelessWidget {
@@ -10,31 +10,100 @@ class HomeScreen extends StatelessWidget {
     print("rerun");
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            //Image
-            Image.network("https://picsum.photos/200/300",
-            width: 200,
-            height: 200,
-            ),
-            //Text
-            Text(
-              "Counter App",
-              style: TextStyle(fontSize: 30, color: Colors.deepPurpleAccent),
-            ),
-            //Button
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.amberAccent,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 300,
+                height: 300,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(150),
+                  color: Colors.amber,
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage("https://picsum.photos/200/300"),
+                  ),
+                ),
               ),
+              //Image
+              // Image.network(
+              //   "https://picsum.photos/200/300",
+              //   width: 200,
+              //   height: 200,
+              // ),
+              //Text
+              Text(
+                "Counter App",
+                style: TextStyle(fontSize: 30, color: Colors.deepPurpleAccent),
+              ),
+              //Button
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.amberAccent,
+                ),
+                onPressed: () {
+                  print("Thichyo");
+                },
+                child: Text("Go to next screen"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CounterScreen extends StatefulWidget {
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  var data = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    print("Build");
+    return Scaffold(
+      appBar: AppBar(title: Text("Counter App")),
+      body: Center(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
               onPressed: () {
-                print("Thichyo");
+                inc();
+                print(data);
               },
-              child: Text("Go to next screen"),
+              child: Text("+"),
+            ),
+            Text(data.toString()),
+            ElevatedButton(
+              onPressed: () {
+                dec();
+                print(data);
+              },
+              child: Text("-"),
             ),
           ],
         ),
       ),
     );
+  }
+
+  void inc() {
+    setState(() {
+      data = data + 1;
+    });
+  }
+
+  void dec() {
+    setState(() {
+      data = data - 1;
+    });
   }
 }
